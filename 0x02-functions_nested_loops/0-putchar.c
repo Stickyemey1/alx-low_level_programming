a64b9730e5ce0e3338e11f889a6609edf52bef87
Stickyemey1 <michaelobioha1989@gmail.com>
2022-09-13 07:46:23 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "alx.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * \n: Print a new line
+ *
+ * Return: 0 On success.
+ **/
+int _putchar(char c)
+{
+	_putchar("\n");
+	return (0);
+}
