2a7399103f292637464de69cc19d9dbf33c68b43
Stickyemey1 <michaelobioha1989@gmail.com>
2022-09-15 17:12:46 -0700

    The shortest distance between two points is a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
  * print_line - a function that draws a straight line in the terminal.
  * @n: the number of character underscore to be printed
  *
  * Return: void
- */
+ **/
 void print_line(int n)
 {
 	while(n > 0)
