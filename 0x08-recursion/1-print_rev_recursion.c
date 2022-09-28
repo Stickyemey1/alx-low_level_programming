536aaf6da0fdeba0f73ba8e6ef88b2dc1a7dabbd
Stickyemey1 <michaelobioha1989@gmail.com>
2022-09-28 09:41:27 -0700

    Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * _prints_rev_recursion - A function to print a string in reverse
+ * @s: input
+ * Return: string
+ */
+
+void prints_rev_recursion(char *s)
+{
+	if (*s !== '\0')
+	{
+		_putchar(*s);
+		return;
+	}
+	_putchar('\n');
+	_prints_rev_reccursion(s + 1);
+}
