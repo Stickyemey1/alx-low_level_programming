91e97c27eb007e3f62ca3398ec131d24d3a053cd
Stickyemey1 <michaelobioha1989@gmail.com>
2022-09-28 09:06:37 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* the prototype of the function _putchar */
+int _putchar(char c)
+/* prototype for all the other functions */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
