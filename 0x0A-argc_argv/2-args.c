1fb92a611ae12da985c1bd3d79bbc9b2c2d93d9b
Stickyemey1 <michaelobioha1989@gmail.com>
2022-09-30 16:30:45 -0700

    The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - a function that multiplies two number
- * @argc: a argument counter
- * @argv: a pointer point to argument string
+ * main - a function that prints all arguments it reveive
+ * @argc: a arguments counter
+ * @argv: an array of pointer point to each argument string
  *
- * Return: return 0 on success, -1 if failure
+ * Return: return 0 on success
  */
-int main(int argc, char *argv[])
-{
-	int res1, res2;
 
-	res1 = res2 = 0;
-	if (argc == 3)
-	{
-		res1 = atoi(argv[1]);
-		res2 = atoi(argv[2]);
-		printf("%d\n", res1 * res2);
-		return (0);
-	}
-	else
-		printf("Error\n");
-	return (-1);
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
 }
