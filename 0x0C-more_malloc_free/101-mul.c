99937a5b5d1e23f3cc4a6e44b4392ba103829368
Stickyemey1 <michaelobioha1989@gmail.com>
2022-10-06 16:24:59 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -29,7 +29,7 @@ void _is_zero(char *argv[])
 
 	if (isn1 == 1 || isn2 == 1)
 	{
-		printf("0\n");
+		_putchar("0\n");
 		exit(0);
 	}
 }
@@ -66,7 +66,7 @@ int _checknum(char *argv[], int n)
 	for (ln = 0; argv[n][ln]; ln++)
 		if (!isdigit(argv[n][ln]))
 		{
-			printf("Error\n");
+			_putchar("Error\n");
 			exit(98);
 		}
 
@@ -87,11 +87,11 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-		printf("Error\n"), exit(98);
+		_putchar("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
-		printf("Error\n"), exit(98);
+		_putchar("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
@@ -121,6 +121,6 @@ int main(int argc, char *argv[])
 			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-	printf("%s\n", nout);
+	_putchar("%s\n", nout);
 	return (0);
 }
