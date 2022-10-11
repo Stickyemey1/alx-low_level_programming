4adca1e4bf521c7bd04df257c8dba5f9b15c6b77
Stickyemey1 <michaelobioha1989@gmail.com>
2022-10-10 18:14:20 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar("%s\n", __FILE__);
+	return (0);
+}
